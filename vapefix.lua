ffd9c8af483d90261869eb90f3714249e5d59fb3
teamswag1337 <112507962+teamswag1337@users.noreply.github.com>
2022-10-24 11:08:29 +0100

    Update vapefix.lua

--- vapefix.lua
+++ vapefix.lua
@@ -7,8 +7,12 @@ local Mouse = LocalPlayer:GetMouse()
 local PresetColor = Color3.fromRGB(44, 120, 224)
 local CloseBind = Enum.KeyCode.RightControl
 
+if game.CoreGui:FindFirstChild("vapeuireal") then
+   game.CoreGui.vapeuireal:Destroy()    
+end
+
 local ui = Instance.new("ScreenGui")
-ui.Name = "ui"
+ui.Name = "vapeuireal"
 ui.Parent = game.CoreGui
 ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
